104ff92abe73fdb3a1eab843f175a4191f078c2b
MockingJay <j.themocking.j@gmail.com>
2025-06-18 07:45:03 +0930

    Delete delete_venv.sh

--- delete_venv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove the Python virtual environment if it exists
-if [ -d .venv ]; then
-  rm -rf .venv
-  echo "Virtual environment removed."
-else
-  echo "No virtual environment found."
-fi
